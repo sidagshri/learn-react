f396d4e7f14fd246ce88fbaca5302a6e7a600c00
Shridhar Agarwal <shridhar.agarwal@citrix.com>
2023-08-27 10:17:02 +0530

    Rendering dynamic list and conditional component rendering - add new expense to initial expense list - make filter work on expense list

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
+const initialExpenses = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 const App = () => {
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet Paper",
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: "e3",
-      title: "Car Insurance",
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: "e4",
-      title: "New Desk (Wooden)",
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
-
+  const [expenses, setExpenses] = useState(initialExpenses);
   // return React.createElement(
   //   'div',
   //   {},
@@ -33,9 +33,13 @@ const App = () => {
   //   React.createElement(Expenses, { items: expenses })
   // );
 
+  const onSaveExpenseData = (expense) => {
+    setExpenses((prevExpenses) => [expense, ...prevExpenses]);
+  };
+
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense={onSaveExpenseData} />
       <Expenses items={expenses} />
     </div>
   );
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,40 +2,64 @@ import React, { useState } from "react";
 
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
-  // const [expenseTitle, setExpenseTitle] = useState("");
-  // const [expenseAmount, setExpenseAmount] = useState("");
-  const [userInput, setUserInput] = useState({
-    expenseTitle: "",
-    expenseAmount: "",
-    expenseDate: "",
-  });
+const ExpenseForm = (props) => {
+  const [expenseTitle, setExpenseTitle] = useState("");
+  const [expenseAmount, setExpenseAmount] = useState("");
+  const [expenseDate, setExpenseDate] = useState("");
+
   const handleTitleChange = (event) => {
-    console.log(event.target.value);
-    setUserInput({
-      ...userInput,
-      expenseTitle: event.target.value,
+    setExpenseTitle(event.target.value);
+  };
+  const handleAmountChange = (event) => {
+    setExpenseAmount(event.target.value);
+  };
+  const handleDateChange = (event) => {
+    setExpenseDate(event.target.value);
+  };
+
+  const handleOnSubmit = (event) => {
+    console.log(event.target);
+    event.preventDefault();
+    props.onSaveExpenseData({
+      title: expenseTitle,
+      amount: expenseAmount,
+      date: new Date(expenseDate),
     });
   };
 
   return (
-    <form>
+    <form onSubmit={handleOnSubmit}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
           <input
+            name="expenseTitle"
             type="text"
             onChange={handleTitleChange}
-            value={userInput.expenseTitle}
+            value={expenseTitle}
           />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            name="expenseAmount"
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={handleAmountChange}
+            value={expenseAmount}
+          />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" max="2022-12-31" />
+          <input
+            name="expenseDate"
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            onChange={handleDateChange}
+            value={expenseDate}
+          />
         </div>
       </div>
       <div className="new-expense__actions">
